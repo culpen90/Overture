bd480de9eb7cf72117d4e23b08222346a9801a4a
culpen90 <sinnomine@duck.com>
2026-07-10 12:36:16 -0400

    Support Swift 5.10 package tooling

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 
@@ -20,6 +20,5 @@ let package = Package(
             dependencies: ["Overture"],
             path: "Tests/OvertureTests"
         )
-    ],
-    swiftLanguageModes: [.v5]
+    ]
 )
